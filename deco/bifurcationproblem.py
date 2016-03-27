fc7ec135147b52062498cec98ee35cf205888ded
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2016-03-27 13:22:23 +0100

    An initial sketch of the BifurcationProblem class and the tasks to be sent to the teams.

---
+++ deco/bifurcationproblem.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+import dolfin
+
+class BifurcationProblem(object):
+    """
+    A base class for bifurcation problems.
+
+    This object is overridden by the user to implement his particular
+    problem.
+    """
+
+    def mesh(self, comm):
+        """
+        This method loads/builds the mesh with a given communicator.
+
+        *Arguments*
+          comm (MPI communicator)
+            The MPI communicator to use in building the mesh.
+
+            Typically, the MPI communicator passed in here will be shared among
+            a small number of the processors (called a team). Each team solves
+            a PDE independently.
+        *Returns*
+          mesh (:py:class:`dolfin.Mesh`)
+        """
+        raise NotImplementedError
+
+    def function_space(self, mesh):
+        """
+        This method creates the function space for the prognostic variables
+        of the problem.
+
+        *Arguments*
+          mesh (:py:class:`dolfin.Mesh`)
+            The mesh to use.
+        *Returns*
+          functionspace (:py:class:`dolfin.FunctionSpace`)
+        """
+        raise NotImplementedError
+
+    def parameters(self):
+        """
+        This method returns a list of tuples. Each tuple contains
+        (Constant, asciiname, symbol). For example, if there is one
+        parameter
+
+        lmbda = Constant(...)
+
+        to be varied, this routine should return
+
+        [(lmbda, "lambda", "λ")]
+
+        You may need to add
+
+        # -*- coding: utf-8 -*-
+
+        to the top of the Python script to use UTF characters.
+
+        Wherever other methods take in parameters, it is as a tuple
+        of :py:class:`dolfin.Constant`s, in the same order as returned
+        by this method.
+
+        *Returns*
+          params
+            A list of [(Constant, asciiname, symbol), ...]
+        """
+        raise NotImplementedError
+
+    def residual(self, state, params, test):
+        """
+        This method defines the PDE to be solved: if you would solve the
+        PDE via
+
+        solve(F == 0, state, ...)
+
+        then this method should return F.
+
+        The parameters will be varied internally by the continuation algorithm, i,e.
+        this will not be called multiple times for multiple parameters.
+
+        *Arguments*
+          state (:py:class:`dolfin.Function`)
+            a Function in the FunctionSpace
+          params (tuple of :py:class:`dolfin.Constant`)
+            the parameters to use, in the same order returned by parameters()
+          test  (:py:class:`dolfin.TestFunction`)
+            the test function to use in defining the residual
+        *Returns*
+          residual (:py:class:`ufl.form.Form`)
+        """
+        raise NotImplementedError
+
+    def boundary_conditions(self, params):
+        """
+        This method returns a list of DirichletBC objects to impose on the
+        problem.
+
+        *Arguments*
+          params (list of :py:class:`dolfin.Constant`)
+            the parameters to use, in the same order returned by parameters()
+        *Returns*
+          bcs (list of :py:class:`dolfin.DirichletBC`)
+        """
+        raise NotImplementedError
+
+    def functionals(self):
+        """
+        This method returns a list of functions. Each function
+        J is called via
+
+          j = J(state, params)
+
+        and should return a float.
+
+        For example, this routine might consist of
+
+        def functionals(self):
+            def l2norm(state, param):
+                return assemble(inner(state, state)*dx)**0.5
+            return [l2norm]
+
+        *Returns*
+          functionals (list of callables)
+        """
+        raise NotImplementedError
+
+    def guesses(self, oldparams, oldstates, newparams):
+        """
+        Given the solutions oldstates corresponding to the parameter
+        values oldparams, construct a list of guesses for the parameter
+        values newparams.
+
+        In the simplest case, this just returns oldstates again.
+
+        There is one special case that must be handled. If oldparams = None
+        and len(oldstates) == 0, then this routine should return the initial
+        guesses to be used for the initial solve (when no solutions are available).
+
+        *Arguments*
+          oldparams (tuple of :py:class:`dolfin.Constant`)
+            old parameters to use, in the same order returned by parameters()
+          oldstates (list of :py:class:`dolfin.Function`)
+            solutions known at the old parameter values
+          newparams (tuple of :py:class:`dolfin.Constant`)
+            new parameters to use, in the same order returned by parameters()
+
+        *Returns*
+          newguesses (list of :py:class:`dolfin.Function`)
+            guesses for the new parameter values
+        """
+        raise NotImplementedError
+
+    def number_solutions(self, params):
+        """
+        If the number of solutions for a given set of parameters is known,
+        then this function can return it. In this case the continuation algorithm
+        will stop looking when it has found that many solutions. Otherwise the
+        routine should return float("inf").
+
+        *Arguments*
+          params (tuple of :py:class:`dolfin.Constant`)
+            parameters to use, in the same order returned by parameters()
+
+        *Returns*
+          nsolutions (int)
+            number of solutions, known from analysis, or float("inf")
+        """
+        return float("inf")
+
+    def inner_product(self, state1, state2):
+        """
+        This method computes the inner product of two vectors in the Hilbert
+        space defined in the function_space method.
+
+        This is used to define the norm used in deflation, and to test two
+        functions for equality, among other things.
+
+        The default is
+            def inner_product(self, state1, state2)
+                return inner(state1, state2)*dx
+
+        *Arguments*
+        """
+        return dolfin.inner(state1, state2)*dolfin.dx
+
---
+++ deco/tasks.py
@@ -0,0 +1,56 @@
+class Task(object):
+    """
+    A base class for Tasks.
+    """
+    pass
+
+class QuitTask(Task):
+    """
+    A task indicating the slave should quit.
+    """
+    pass
+
+class ContinuationTask(Task):
+    """
+    A base task for continuing a known branch.
+
+    *Arguments*
+      taskid (int)
+        Global identifier for this task
+      oldparams (dict)
+        Parameter values to continue from
+      branchid (int)
+        Which branch to continue (int)
+      newparams (dict)
+        Parameter values to continue to
+    """
+    def __init__(self, taskid, oldparams, branchid, newparams):
+        self.taskid    = taskid
+        self.oldparams = oldparams
+        self.branchid  = branchid
+        self.newparams = newparams
+
+class DeflationTask(Task)
+    """
+    A task that seeks new, unknown solutions for a given parameter
+    value.
+
+    *Arguments*
+      taskid (int)
+        Global identifier for this task
+      oldparams (dict)
+        Parameter values to continue from
+      branchid (int)
+        Which branch to continue (int)
+      newparams (dict)
+        Parameter values to continue to
+      knownbranches (list)
+        Branch ids that have known solutions for newparams
+
+    """
+    def __init__(self, taskid, oldparams, branchid, newparams, knownbranches):
+        self.taskid    = taskid
+        self.oldparams = oldparams
+        self.branchid  = branchid
+        self.newparams = newparams
+        self.knownbranches = knownbranches
