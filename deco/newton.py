e1872254f8c5e1cf26707e47d6ce799bd0823999
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2016-03-29 15:34:16 +0100

    Initial implementation of shifted deflation operator and Newton solver that uses it.

---
+++ deco/newton.py
@@ -0,0 +1,59 @@
+from dolfin import *
+from petsc4py import PETSc
+
+def printnorm(i, n):
+    print "%3d SNES Function norm %1.15e" % (i, n)
+
+def newton(F, y, bcs, deflation=None, prefix="", printnorm=printnorm):
+
+    # Fetch some SNES options from the PETSc dictionary
+    if len(prefix) > 0:
+        if prefix[-1] != "_":
+            prefix = prefix + "_"
+
+    opts = PETSc.Options()
+    atol = opts.getReal(prefix + "snes_atol", default=1.0e-8)
+    rtol = opts.getReal(prefix + "snes_rtol", default=1.0e-8)
+    maxits = opts.getInt(prefix + "snes_max_it", default=100)
+    monitor = opts.getBool(prefix + "snes_monitor", default=True)
+
+    def norm(F, y, bcs):
+        b = assemble(F)
+        [bc.apply(b, y.vector()) for bc in bcs]
+        return b.norm("l2")
+
+    [bc.apply(y.vector()) for bc in bcs]
+
+    dy = Function(y.function_space())
+    J = derivative(F, y, dy)
+    hbcs = [DirichletBC(bc) for bc in bcs]; [hbc.homogenize() for hbc in hbcs]
+    i = 0
+
+    n0 = norm(F, y, bcs)
+    n  = n0
+    if monitor: printnorm(i, n)
+
+    success = False
+
+    while True:
+        if i >= maxits:  break
+        if n <= atol:    success = True; break
+        if n/n0 <= rtol: success = True; break
+
+        dy.vector().zero()
+
+        solve(J + F == 0, dy, hbcs)
+
+        if deflation is not None:
+            sigma = deflation.derivative(y).inner(dy.vector()) / deflation.evaluate(y)
+            dy.assign((1.0/(1 + sigma)) * dy)
+            # is that it?
+
+        y.assign(y + dy)
+
+        i = i + 1
+        n = norm(F, y, bcs)
+        if monitor: printnorm(i, n)
+
+    return success
+
--- deco/operatordeflation.py
+++ deco/operatordeflation.py
@@ -1,8 +1,78 @@
+from dolfin import *
+
+def empty_vector(model):
+  """
+  Make a zero vector from a model, including parallel layout.
+  """
+  b = Vector(model)
+  b.zero()
+  return b
+
 class DeflationOperator(object):
-    pass
+    """
+    Base class for deflation operators.
+    """
+    def deflate(self, roots):
+        self.roots = roots
+
+    def evaluate(self):
+        raise NotImplementedError
+
+    def derivative(self):
+        raise NotImplementedError
 
 class ShiftedDeflation(object):
+    """
+    The shifted deflation operator presented in doi:10.1137/140984798.
+    """
     def __init__(self, problem, power, shift):
         self.problem = problem
         self.power = power
         self.shift = shift
+        self.roots = []
+
+    def normsq(self, y, root):
+        return problem.inner_product(y - root, y - root)
+
+    def evaluate(self, y):
+        m = 1.0
+        for normsq in [assemble(self.normsq(y, root)) for root in self.roots]:
+            factor = normsq**(-self.power/2.0) + self.shift
+            m *= factor
+
+        return m
+
+    def derivative(self, y):
+        if len(self.roots) == 0:
+            deta = empty_vector(y.vector())
+            deta.zero()
+            return deta
+
+        p = self.power
+        factors  = []
+        dfactors = []
+        dnormsqs = []
+        normsqs  = []
+
+        for root in self.roots:
+            form = self.normsq(y, root)
+            normsqs.append(assemble(form))
+            dnormsqs.append(assemble(derivative(form, y)))
+
+        for normsq in normsqs:
+            factor = normsq**(-p/2.0) + self.shift
+            dfactor = (-p/2.0) * normsq**((-p/2.0) - 1.0)
+
+            factors.append(factor)
+            dfactors.append(dfactor)
+
+        eta = product(factors)
+
+        deta = empty_vector(y.vector())
+        deta.zero()
+
+        for (solution, factor, dfactor, dnormsq) in zip(self.roots, factors, dfactors, dnormsqs):
+            deta.axpy((eta/factor)*dfactor, dnormsq)
+
+        return deta
+
