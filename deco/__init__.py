296d98fc148cff309fb26c81b43df09e7fb0e074
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2016-03-31 20:56:39 +0100

    Rename io to iomodule because it breaks IPython, meld, etc

--- deco/__init__.py
+++ deco/__init__.py
@@ -4,4 +4,4 @@ dolfin.set_log_level(dolfin.ERROR)
 from numpy                import arange, linspace
 from bifurcationproblem   import BifurcationProblem
 from deflatedcontinuation import DeflatedContinuation
-from io                   import IO, FileIO
+from iomodule             import IO, FileIO
