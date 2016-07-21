8b51aaf89fb5b4cad796b21d68d0895914fa55fa
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2016-07-21 10:35:11 +0100

    We no longer need MPI_THREAD_MULTIPLE (thank god, since it's not available on ARCHER).

--- defcon/__init__.py
+++ defcon/__init__.py
@@ -1,17 +1,4 @@
-from mpi4py import rc
-rc.initialize = False
-
 from mpi4py import MPI
-if not MPI.Is_initialized():
-    ret = MPI.Init_thread(required=MPI.THREAD_MULTIPLE)
-    if ret != MPI.THREAD_MULTIPLE and MPI.COMM_WORLD.size > 1:
-        print "Error: defcon needs MPI_THREAD_MULTIPLE support. Update your version of MPI."
-        assert ret == MPI.THREAD_MULTIPLE
-else:
-    print "Warning: defcon did not initialize MPI."
-    print "Please make sure that whoever did initialize MPI initialized with"
-    print "MPI_Init_thread(MPI_THREAD_MULTIPLE)"
-    print "and made sure that MPI offered that level of support."
 
 try:
     import matplotlib
@@ -24,5 +11,5 @@ dolfin.set_log_level(dolfin.ERROR)
 
 from numpy                import arange, linspace
 from bifurcationproblem   import BifurcationProblem
-from defcon                 import DeflatedContinuation
+from defcon               import DeflatedContinuation
 from iomodule             import IO, FileIO
