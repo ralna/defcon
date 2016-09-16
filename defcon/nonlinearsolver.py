3a13c1cb2775060ed112cfd2fda9b013cffe5393
Robert C. Kirby <robert_kirby@baylor.edu>
2016-09-16 16:53:26 -0500

    reorganized firedrake examples into single directory.  Also added missing nonlinearsolver.py

---
+++ defcon/nonlinearsolver.py
@@ -0,0 +1,27 @@
+import backend
+if backend.__name__ == "dolfin":
+    from backend import PETScSNESSolver, as_backend_type
+    
+    # dolfin lacks a high-level snes frontend like Firedrake,
+    # so we're going to put one here and build up what we need
+    # to make things happen.
+    class SNUFLSolver(object):
+        def __init__(self, problem, prefix="", **kwargs):
+            y = problem.u
+            comm = y.function_space().mesh().mpi_comm()
+            low_level_solver = PETScSNESSolver(comm)
+            low_level_solver.init(problem, y.vector())
+
+            snes = low_level_solver.snes()
+            snes.setOptionsPrefix(prefix)
+            
+            self.problem=problem
+            self.low_level_solver=low_level_solver
+            self.snes = low_level_solver.snes()
+
+        def solve(self):
+            # Need a copy for line searches etc. to work correctly.
+            x = self.problem.u.copy(deepcopy=True)
+            self.snes.solve(None, as_backend_type(x.vector()).vec())
+            
+            
