5156e8cab5c57b9afce55bba854071b6557a3121
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2016-09-17 21:58:45 +0200

    Clean up Rob's whitespace

--- defcon/newton.py
+++ defcon/newton.py
@@ -8,7 +8,7 @@ from numpy import isnan
 
 if backend.__name__ == "dolfin":
     from backend import PETScOptions, PETScVector
-    
+
     def getEdy(deflation, y, dy):
         dy_vec = PETScVector(dy)
         return -deflation.derivative(y).inner(dy_vec)
@@ -39,35 +39,35 @@ class DeflatedKSP(object):
         self.deflation = deflation
         self.y = y
         self.ksp = ksp
-        
+
     def solve(self, ksp, b, dy_pet):
         # Use the inner ksp to solve the original problem
         self.ksp.solve(b, dy_pet)
         deflation = self.deflation
-        
+
         if deflation is not None:
             Edy = getEdy(deflation, self.y, dy_pet)
-                
+
             minv = 1.0 / deflation.evaluate(self.y)
             tau = (1 + minv*Edy/(1 - minv*Edy))
             dy_pet.scale(tau)
 
         ksp.setConvergedReason(self.ksp.getConvergedReason())
-        
+
 def newton(F, y, bcs, problemclass, solverclass,
            teamno, deflation=None, prefix=""):
     comm = y.function_space().mesh().mpi_comm()
     problem = problemclass(F, y, bcs)
-    
+
     solver = solverclass(problem, prefix=prefix)
-    
+
     snes = solver.snes
-    
+
     # all of this is likely defcon-specific and so shouldn't go
     # into the (general-purpose) SNUFLSolver.
     cancelSnesMonitor(prefix)
     snes.incrementTabLevel(teamno*2)
-    
+
     oldksp = snes.ksp
     oldksp.incrementTabLevel(teamno*2)
     defksp = DeflatedKSP(deflation, y, oldksp)
@@ -75,12 +75,12 @@ def newton(F, y, bcs, problemclass, solverclass,
     snes.ksp.pc.setType('none')
     snes.ksp.setOperators(*oldksp.getOperators())
     snes.ksp.setUp()
-    
+
     try:
         solver.solve()
     except:
         pass
-    
+
     success = snes.getConvergedReason() > 0
     return success
 
