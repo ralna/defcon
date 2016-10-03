e13a1aebedd1c9d32f53a39026f4a1b63977a183
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2016-10-03 11:36:35 +0100

    Refactor the code for importing backends.

--- defcon/__init__.py
+++ defcon/__init__.py
@@ -6,41 +6,9 @@ try:
 except ImportError:
     pass
 
-import sys
-if "dolfin" in sys.modules and "firedrake" not in sys.modules:
-    # dolfin imported, no firedrake
-    import dolfin
-    assert dolfin.has_petsc4py()
-
-    # Check dolfin version
-    if dolfin.__version__.startswith("1") or dolfin.__version__.startswith("2016.1.0"):
-        raise ImportError("Your version of DOLFIN is too old. DEFCON needs the development version of DOLFIN, 2016.2.0+.")
-
-    dolfin.set_log_level(dolfin.ERROR)
-    sys.modules['backend'] = dolfin
-
-elif "firedrake" in sys.modules and "dolfin" not in sys.modules:
-    # firedrake imported, no dolfin
-    import firedrake
-    sys.modules['backend'] = firedrake
-
-    firedrake.parameters["assembly_cache"]["enabled"] = False
-    firedrake.parameters["pyop2_options"]["lazy_evaluation"] = False
-
-elif "firedrake" in sys.modules and "dolfin" in sys.modules:
-    # both loaded, don't know what to do
-    raise ImportError("Import exactly one of dolfin or firedrake before importing defcon.")
-
-else: # nothing loaded, default to DOLFIN
-    import dolfin
-    assert dolfin.has_petsc4py()
-
-    # Check dolfin version
-    if dolfin.__version__.startswith("1") or dolfin.__version__.startswith("2016.1.0"):
-        raise ImportError("Your version of DOLFIN is too old. DEFCON needs the development version of DOLFIN, 2016.2.0+.")
-
-    dolfin.set_log_level(dolfin.ERROR)
-    sys.modules['backend'] = dolfin
+import backends
+backends.import_backend()
+import backend
 
 from numpy                import arange, linspace
 from bifurcationproblem   import BifurcationProblem
@@ -48,7 +16,6 @@ from defcon               import DeflatedContinuation
 from iomodule             import IO, SolutionIO, BranchIO
 from operatordeflation    import ShiftedDeflation
 
-import backend
 if backend.__name__ == "dolfin":
     from nonlinearsolver import SNUFLSolver
     from Probe import Probe # borrowed from Mikael Mortensen's excellent fenicstools
---
+++ defcon/backends.py
@@ -0,0 +1,43 @@
+def import_backend():
+    import sys
+
+    use_dolfin = True
+    use_firedrake = False
+
+    if "dolfin" in sys.modules and "firedrake" not in sys.modules:
+        use_dolfin = True
+
+    elif "firedrake" in sys.modules and "dolfin" not in sys.modules:
+        use_dolfin = False
+        use_firedrake = True
+
+    elif "firedrake" in sys.modules and "dolfin" in sys.modules:
+        # both loaded, don't know what to do
+        raise ImportError("Import exactly one of dolfin or firedrake before importing defcon.")
+
+    else: # nothing loaded, default to DOLFIN
+        use_dolfin = True
+
+    if use_dolfin:
+        import dolfin
+        assert dolfin.has_petsc4py()
+
+        # Check dolfin version
+        if dolfin.__version__.startswith("1") or dolfin.__version__.startswith("2016.1.0"):
+            raise ImportError("Your version of DOLFIN is too old. DEFCON needs the development version of DOLFIN, 2016.2.0+.")
+
+        dolfin.set_log_level(dolfin.ERROR)
+        sys.modules['backend'] = dolfin
+
+        dolfin.parameters["form_compiler"]["representation"] = "uflacs"
+        dolfin.parameters["form_compiler"]["optimize"]     = True
+        dolfin.parameters["form_compiler"]["cpp_optimize"] = True
+        dolfin.parameters["form_compiler"]["cpp_optimize_flags"] = "-O3 -ffast-math -march=native"
+
+    elif use_firedrake:
+        # firedrake imported, no dolfin
+        import firedrake
+        sys.modules['backend'] = firedrake
+
+        firedrake.parameters["assembly_cache"]["enabled"] = False
+        firedrake.parameters["pyop2_options"]["lazy_evaluation"] = False
