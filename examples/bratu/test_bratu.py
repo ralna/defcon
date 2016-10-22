5006d7f3a071f108103379dc985a5dfe5215cd89
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2016-10-22 23:12:45 +0100

    Two more tests

---
+++ examples/bratu/test_bratu.py
@@ -0,0 +1,22 @@
+from defcon import *
+from bratu import BratuProblem
+import json
+
+def test_bratu():
+    problem = BratuProblem()
+    dc = DeflatedContinuation(problem, teamsize=1, clear_output=True)
+    dc.run(free={"lambda": list(arange(0.0, 3.6, 0.01)) + [3.6]})
+
+    ac = ArclengthContinuation(problem, teamsize=1)
+    ac.run(params=(0.5,), free="lambda", ds=0.1, sign=+1, bounds=(0.01, 3.6), branchids=[0])
+
+    io = problem.io()
+    io.setup(problem.parameters(), problem.functionals(), None)
+
+    assert len(io.known_branches((3.0,))) == 2
+
+    data = json.load(open("output/arclength/params-lambda=5.000000000000000e-01-freeindex-0-branchid-0-ds-1.00000000000000e-01.json", "r"))
+    x = [entry[0] for entry in data]
+    y = [entry[1][0] for entry in data]
+    assert len(x) == len(y)
+    assert len(x) > 0
---
+++ examples/painleve/test_painleve.py
@@ -0,0 +1,12 @@
+from defcon import *
+from painleve import PainleveProblem
+
+def test_allen_cahn():
+    problem = PainleveProblem()
+    dc = DeflatedContinuation(problem, teamsize=1, verbose=True, clear_output=True)
+    dc.run(free={"a": [6.0]})
+
+    io = problem.io()
+    io.setup(problem.parameters(), problem.functionals(), None)
+
+    assert len(io.known_branches((6.0,))) == 2
