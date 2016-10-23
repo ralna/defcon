071bf167299b8e6650e8089f47375006c639930c
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2016-10-23 17:48:09 +0100

    Clear the PETSc Options between test runs

--- conftest.py
+++ conftest.py
@@ -1,10 +1,20 @@
 import os
 import functools
+from petsc4py import PETSc
 
 def pytest_runtest_setup(item):
     """
-    Execute each test in the directory where the test file lives.
+    - Execute each test in the directory where the test file lives.
+    - Clear the PETSc options
     """
+
+    opts = PETSc.Options()
+    # should just do
+    # opts.clear()
+    # but it doesn't work!
+    for key in opts.getAll():
+        opts.delValue(key)
+
     starting_directory = os.getcwd()
     test_directory = os.path.dirname(str(item.fspath))
     os.chdir(test_directory)
