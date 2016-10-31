8feb5245743284ba3dcaf8ef01b80c6481037602
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2016-10-31 15:34:02 +0000

    Winged cusp example

---
+++ examples/wingedcusp/wingedcusp.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+import sys
+from   math import floor
+
+from defcon import *
+from dolfin import *
+
+import matplotlib.pyplot as plt
+
+from petsc4py import PETSc
+
+parameters["form_compiler"]["representation"] = "quadrature"
+
+class WingedCuspProblem(BifurcationProblem):
+    def mesh(self, comm):
+        return UnitIntervalMesh(comm, 2)
+
+    def function_space(self, mesh):
+        return FunctionSpace(mesh, "R", 0)
+
+    def parameters(self):
+        lmbda = Constant(0)
+
+        return [(lmbda, "lambda", r"$\lambda$")]
+
+    def residual(self, x, params, v):
+        lmbda = params[0]
+
+        F = (
+              x**3*v*dx
+            - 2*lmbda*x*v*dx
+            + lmbda**2*v*dx
+            - 2*lmbda*v*dx
+            + 1*v*dx
+            )
+
+        return F
+
+    def boundary_conditions(self, V, params):
+        return []
+
+    def functionals(self):
+        def value(x, params):
+            return x.vector().array()[0]
+
+        return [(value, "value", r"$x$")]
+
+    def number_initial_guesses(self, params):
+        return 1
+
+    def initial_guess(self, V, params, n):
+        return interpolate(Constant(0.1), V)
+
+    def number_solutions(self, params):
+        lmbda = params[0]
+        if 0.3 < lmbda < 4:
+            return 3
+        else:
+            return 1
+
+    def solver_parameters(self, params, klass):
+        args = {
+               "snes_max_it": 20,
+               "snes_atol": 1.0e-9,
+               "snes_rtol": 0.0,
+               "snes_monitor": None,
+               "ksp_type": "preonly",
+               "pc_type": "lu",
+               }
+        return args
+
+if __name__ == "__main__":
+    dc = DeflatedContinuation(problem=WingedCuspProblem(), teamsize=1, verbose=True, clear_output=True)
+    dc.run(values={"lambda": linspace(-3, 6, 91)})
