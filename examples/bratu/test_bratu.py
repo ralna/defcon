531d0d48af7816955a9a220eba15dc576db487fc
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2016-11-07 11:20:17 +0000

    Shorten the tests

--- examples/bratu/test_bratu.py
+++ examples/bratu/test_bratu.py
@@ -5,10 +5,10 @@ import json
 def test_bratu():
     problem = BratuProblem()
     dc = DeflatedContinuation(problem, teamsize=1, clear_output=True)
-    dc.run(values={"lambda": list(arange(0.0, 3.6, 0.01)) + [3.6]})
+    dc.run(values={"lambda": list(arange(0.0, 3.6, 0.1)) + [3.6]})
 
     ac = ArclengthContinuation(problem, teamsize=1)
-    ac.run(params=(0.5,), free="lambda", ds=0.1, sign=+1, bounds=(0.01, 3.6), branchids=[0])
+    ac.run(params=(0.5,), free="lambda", ds=0.5, sign=+1, bounds=(0.01, 3.6), branchids=[0])
 
     io = problem.io()
     io.setup(problem.parameters(), problem.functionals(), None)
@@ -17,7 +17,7 @@ def test_bratu():
     if backend.comm_world.rank == 0:
         assert len(io.known_branches((3.0,))) == 2
 
-        data = json.load(open("output/arclength/params-lambda=5.000000000000000e-01-freeindex-0-branchid-0-ds-1.00000000000000e-01.json", "r"))
+        data = json.load(open("output/arclength/params-lambda=5.000000000000000e-01-freeindex-0-branchid-0-ds-5.00000000000000e-01.json", "r"))
         x = [entry[0] for entry in data]
         y = [entry[1][0] for entry in data]
         assert len(x) == len(y)
--- examples/yamabe/test_yamabe.py
+++ examples/yamabe/test_yamabe.py
@@ -1,8 +1,13 @@
 from defcon import *
 from yamabe import YamabeProblem
 
+# Don't do the extra (failed) search
+class TestYamabeProblem(YamabeProblem):
+    def number_solutions(self, params):
+        return 7
+
 def test_yamabe():
-    problem = YamabeProblem()
+    problem = TestYamabeProblem()
     deflation = ShiftedDeflation(problem, power=1, shift=1.0e-2)
     dc = DeflatedContinuation(problem, deflation=deflation, teamsize=1, verbose=True, clear_output=True)
     dc.run(values={"a": [8.0]})
