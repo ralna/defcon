82c1056913acafe0385f6018440e650115a2f058
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2016-11-23 10:22:41 +0000

    Generalize the make_pvd script to BranchIO

--- bin/make_pvd.py
+++ bin/make_pvd.py
@@ -2,27 +2,46 @@
 
 # A script that converts saved data to PVD.
 # Use like
-# python make_pvd.py /path/to/my/problem.py /path/to/output/directory
+# python make_pvd.py /path/to/my/problem.py /path/to/output/directory values
+# e.g.
+# python make_pvd.py /path/to/my/problem.py /path/to/output/directory "(0, 0)"
 
 import sys
 import scriptcommon
+from ast import literal_eval
 
 probpath = sys.argv[1]
 outputdir = sys.argv[2]
+if outputdir.endswith("/"): outputdir = outputdir[:-1]
+values = literal_eval(sys.argv[3])
+if isinstance(values, float): values = (values,)
 
 problem = scriptcommon.fetch_bifurcation_problem(probpath)
 
+io = problem.io(outputdir)
+
 import glob
 import backend
+import defcon.parametertools
+import defcon.newton
+import gc
+import os
 
 mesh = problem.mesh(backend.comm_world)
 Z = problem.function_space(mesh)
-pvd = backend.File(outputdir + "/roots.pvd")
-f = backend.Function(Z, name="Solution")
+functionals = problem.functionals()
+params = problem.parameters()
+consts = [x[0] for x in params]
 
-for root in sorted(glob.glob(outputdir + "/solution-*.h5")):
-    with backend.HDF5File(backend.comm_world, root, 'r') as g:
-        g.read(f, "/solution")
-    problem.save_pvd(f, pvd)
+io.setup(params, functionals, Z)
+params = consts
 
-print "Wrote to " + outputdir + "/roots.pvd"
+filename = os.path.join(outputdir, "viz", "values-%s.pvd" % (sys.argv[3],))
+pvd = backend.File(filename)
+branches = io.known_branches(values)
+solutions = io.fetch_solutions(values, branches)
+for solution in solutions:
+    solution.rename("Solution", "Solution")
+    problem.save_pvd(solution, pvd)
+
+print "Wrote to " + filename
