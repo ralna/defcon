5f3a818f7942e29f321d736538d1979e2c111496
Jan Blechta <blechta@karlin.mff.cuni.cz>
2017-02-06 17:08:07 +0000

    Get rid of global var in fetch_bifurcation_problem

--- defcon/cli/common.py
+++ defcon/cli/common.py
@@ -1,15 +1,16 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 
 import os
 import imp
 
+from defcon import BifurcationProblem
+
 
 def fetch_bifurcation_problem(path):
-
     cwd = os.getcwd()
-
     probdir = os.path.dirname(path)
-    if len(probdir) > 0: os.chdir(probdir)
+    if len(probdir) > 0:
+        os.chdir(probdir)
 
     # Check if the user's given us a directory or a .py file
     if path.endswith(".py"):
@@ -24,18 +25,31 @@ def fetch_bifurcation_problem(path):
     else:
         raise ValueError("Either specify the .py file or the directory containing it.")
 
+    # Import file
     prob = imp.load_source("prob", probpath)
 
-    # FIXME: Messig up with globals?! Not good for a module
-    globals().update(vars(prob))
-    # Run through each class we've imported and figure out which one inherits from BifurcationProblem.
-    classes = [key for key in globals().keys()]
-    for c in classes:
+    # Run through each class we've imported and figure out which one inherits from BifurcationProblem
+    for v in vars(prob).values():
         try:
-            globals()["bfprob"] = getattr(prob, c)
-            assert issubclass(bfprob, BifurcationProblem) and bfprob is not BifurcationProblem # check whether the class is a subclass of BifurcationProblem, which would mean it's the class we want. 
-            problem = bfprob() # initialise the class.
-            break
-        except Exception: pass
+            # Check if we have what we want
+            assert issubclass(v, BifurcationProblem)
+            assert v is not BifurcationProblem
+            print("Found BifurcationProblem subclass: %s" % v)
+
+            # Now try to initialize it with no arguments
+            try:
+                problem = v()
+            except Exception:
+                print("Failed to init BifurcationProblem subclass: %s " % v)
+                print("Trying other classes...")
+            else:
+                break
+
+        except (AssertionError, TypeError):
+            # Did not pass the requirements, try other classes
+            pass
+    else:
+        # Not good, the loop finished without break
+        raise Exception("Failed to fetch bifurcation problem")
 
     return problem
