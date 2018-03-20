3c636e6f2f50aa924660d7fa919c9bae67dc144f
Patrick Farrell <patrick@pefarrell.org>
2018-03-20 12:00:49 +0000

    Don't rely on deprecated form compiler

--- examples/wingedcusp/wingedcusp.py
+++ examples/wingedcusp/wingedcusp.py
@@ -2,8 +2,6 @@
 from defcon import *
 from dolfin import *
 
-parameters["form_compiler"]["representation"] = "quadrature"
-
 class WingedCuspProblem(BifurcationProblem):
     def mesh(self, comm):
         return UnitIntervalMesh(comm, 2)
