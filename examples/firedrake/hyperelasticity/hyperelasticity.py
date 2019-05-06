04547a0cf0865b704a11954063af19662e2f64da
e <e_medina@g.harvard.edu>
2019-05-06 11:46:44 -0400

    Removing line at the end

--- examples/firedrake/hyperelasticity/hyperelasticity.py
+++ examples/firedrake/hyperelasticity/hyperelasticity.py
@@ -210,4 +210,3 @@ if __name__ == "__main__":
     dc = DeflatedContinuation(problem=HyperelasticityProblem(), teamsize=1, verbose=True, clear_output=True)
     params = list(arange(0.0, 0.2, 0.001)) + [0.2]
     dc.run(values={"eps": params})
-
