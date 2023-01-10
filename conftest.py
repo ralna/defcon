947e243883f1c5b75c97edf02e1ed3b2ab6f0add
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2023-01-10 22:12:25 +0000

    Update pytest conftest.py.

--- conftest.py
+++ conftest.py
@@ -1,13 +1,22 @@
 import os
 import functools
 from petsc4py import PETSc
+import pytest
 
-def pytest_runtest_setup(item):
-    """
-    - Execute each test in the directory where the test file lives.
-    - Clear the PETSc options
-    """
+starting_directory = None
+def setup_module(module):
+    global starting_directory
+    starting_directory = os.getcwd()
+    test_directory = os.path.dirname(str(module.fspath))
+    os.chdir(test_directory)
 
+def teardown_module(module):
+    os.chdir(starting_directory)
+
+def setup_function(function):
+    """ setup any state tied to the execution of the given function.
+    Invoked for every test function in the module.
+    """
     opts = PETSc.Options()
     # should just do
     # opts.clear()
@@ -15,10 +24,3 @@ def pytest_runtest_setup(item):
     for key in opts.getAll():
         opts.delValue(key)
 
-    starting_directory = os.getcwd()
-    test_directory = os.path.dirname(str(item.fspath))
-    os.chdir(test_directory)
-
-    teardown = functools.partial(os.chdir, starting_directory)
-    # There's probably a cleaner way than accessing a private member.
-    item.session._setupstate.addfinalizer(teardown, item)
